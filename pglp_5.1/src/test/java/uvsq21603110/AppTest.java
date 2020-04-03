4156cf9d69a4b2c980c87373efdce5876ed657e2
alexfac <alexandre.sarrazin@ens.uvsq.fr>
2020-04-03 10:45:53 +0200

    First commit

---
+++ pglp_5.1/src/test/java/uvsq21603110/AppTest.java
@@ -0,0 +1,37 @@
+package uvsq21603110;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testInitperso(){
+        Personnel personnel = new Personnel.Builder("Test", "Test", "Testeur").build();
+        assertEquals("Test", personnel.getNom());
+        assertEquals("Test", personnel.getPrenom());
+        assertEquals("Testeur", personnel.getFonction());
+    }
+
+    @Test
+    public void testAddtel(){
+        String Tel = "0130333909";
+        Personnel personnel = new Personnel.Builder("Test", "Test", "Testeur").addTel(Tel).build();
+        assertEquals(personnel.getTel().get(0), Tel);
+    }
+
+    @Test
+    public void testAddnaissance(){
+        LocalDate naissance = LocalDate.now();
+        Personnel personnel = new Personnel.Builder("Test", "Test", "Testeur").addNaissance(naissance).build();
+        assertEquals(personnel.getNaissance(), naissance);
+    }
+}
